8cb18ef41b266a77d300987b5edd0432d3523797
CordeiroAboumrad <cordeiro.aboumrad@gmail.com>
2022-05-12 21:14:26 -0300

    Implementation of table and table lines components

--- atlas-financeiro/src/App.js
+++ atlas-financeiro/src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import TabelaCotacoes from './cotacoes/Cotacoes';
 
-function App() {
+
+const cotacoes = [
+  {data: '11/05/2022', valor: 78.6},
+  {data: '10/05/2022', valor: 75.7},
+  {data: '09/05/2022', valor: 76.51},
+  {data: '08/05/2022', valor: 79.94},
+  {data: '07/05/2022', valor: 80.41},
+  {data: '06/05/2022', valor: 81.9},
+  {data: '05/05/2022', valor: 82.7},
+  {data: '04/05/2022', valor: 83.09},
+  {data: '03/05/2022', valor: 82.95},
+  {data: '02/05/2022', valor: 84.14},
+  {data: '01/05/2022', valor: 82.28},
+]
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TabelaCotacoes cotacoes={cotacoes} />
+    </>
   );
 }
 
