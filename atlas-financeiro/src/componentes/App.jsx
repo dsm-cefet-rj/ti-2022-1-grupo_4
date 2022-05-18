004765258c3a8914e1d4df95d61cb07acf083fa9
CordeiroAboumrad <cordeiro.aboumrad@gmail.com>
2022-05-17 23:15:00 -0300

    Inclusão da parte de Carteira nas rotas

--- atlas-financeiro/src/componentes/App.jsx
+++ atlas-financeiro/src/componentes/App.jsx
@@ -11,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/perfil" element={<Perfil />} />
         <Route path="/detalhamento" element={<Detalhamento />} />
+        <Route path="/carteira" element={<Carteira />} />
       </Routes>
     </Router>
   );
