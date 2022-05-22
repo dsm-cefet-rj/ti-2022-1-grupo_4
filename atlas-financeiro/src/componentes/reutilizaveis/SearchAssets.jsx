3a48586c7c4b93b57955798d7727aa2a50408e3b
CordeiroAboumrad <cordeiro.aboumrad@gmail.com>
2022-05-22 02:41:59 -0300

    Search implementation using useState

---
+++ atlas-financeiro/src/componentes/reutilizaveis/SearchAssets.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+export default function SearchAssets() {
+
+    const [search, setSearch] = useState({});
+
+    function handleChange(e) {
+        setSearch( {...search, [e.target.name]: e.target.value} )
+    }
+
+    function handleSubmit(event) {
+        alert('Valor enviado: ' + search.busca);
+        event.preventDefault();
+    }
+
+    return (
+        <>
+            <div className="search-container">
+                <div className="container-fluid">
+                    <form className="d-flex" onSubmit={handleSubmit}>
+                        <input name='busca' value={search.pesquisa} className="form-control me-2" type="search" placeholder="Procurar por ação/empresa/fii" aria-label="Search" onChange={handleChange} />
+                        <input type='submit' value="Buscar" />
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+}
