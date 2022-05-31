7a33d9a7b7ce9c7b5f6e57f32a622d1c424ae744
CordeiroAboumrad <cordeiro.aboumrad@gmail.com>
2022-05-30 23:59:52 -0300

    Redux implementation for watchlist and minor layout adjustments

--- atlas-financeiro/src/componentes/reutilizaveis/SearchAssets.jsx
+++ atlas-financeiro/src/componentes/reutilizaveis/SearchAssets.jsx
@@ -1,4 +1,4 @@
-import React, { Provider } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 function SearchAssets(props) {
--- atlas-financeiro/src/store.js
+++ atlas-financeiro/src/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import mudaBusca from './componentes/reducers/BuscaSlice'
+import { alteraTicker, alteraCotacao, alteraDropdown } from './componentes/reducers/WatchlistSlice'
+import adicionaWatchlist from './componentes/reducers/WatchlistSlice'
 
 export const store = configureStore({
     reducer: {
-        busca: mudaBusca
+        busca: mudaBusca,
+        ticker: alteraTicker,
+        cotacao: alteraCotacao,
+        dropdown: alteraDropdown,
+        watchlist: adicionaWatchlist
     }
 })
