bee6cfb8227e37337909ce1d67cf4b42a4cabf33
jsuisjuan <juanc.s.delima@gmail.com>
2022-06-10 20:50:31 -0300

    tentativa falha de estilizar o login

--- atlas-financeiro/src/componentes/Login.jsx
+++ atlas-financeiro/src/componentes/Login.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
-function Login() {
-    return (
-        <h2>Esta é a página de login</h2>
-    );
+
+class Login extends React.Component {
+
+    render() {
+        require("./styles/login-signup-styles.css");
+        
+        return ( 
+            <div className='login_component'>
+            
+                <div className='login_container'>
+                    <h2>Log in</h2>
+                    <form>
+                        <div className='login_email_container'>
+                            <div className='login_email_info_container'>
+                                <label for='email'>Email</label>
+                                <span>
+                                    Precisa de uma conta? 
+                                    <span><a href='/sign-up'>Cadastre-se já</a></span>
+                                </span>
+                            </div>
+                            <div className='login_email_input_container'>
+                                <input className='email_input' type='email' />
+                            </div>
+                        </div>
+                    
+                        <div className='login_password_container'>
+                            <div className='login_password_info_container'>
+                                <label for='current-password'>Senha</label>
+                                <span>Amostrar</span>
+                            </div>
+                            <div className='login_password_input_container'>
+                                <input type='password' />
+                            </div>
+                        </div>
+                    
+                        <div className='login_button_container'> 
+                            <div className='login_button'>
+                                <Button variant="outline-dark" className='login_button'>Log in</Button>
+                            </div>
+                            <div className='login_button_forgot_password_container'>
+                                <a href='#'>Esqueceu sua senha?</a>
+                            </div>
+                        </div>
+                    
+                    </form>
+                </div> 
+            </div>    
+        );
+    }
 }
 
 export default Login;
--- atlas-financeiro/src/componentes/SignUp.jsx
+++ atlas-financeiro/src/componentes/SignUp.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 function SignUp() {
     return (
-        <h2>Esta é a página de sign-up</h2>
+        <div className='signup-component'>
+            <div className='signup-container'>
+            <h2>Cadastre-se já</h2>
+                <form>
+                    <div className='login_email_container'>
+                        <div className='login_email_info_container'>
+                            <label for='email'>Email</label>
+                        </div>
+                        <div className='login_email_input_container'>
+                            <input className='email_input' type='email' />
+                        </div>
+                    </div>
+                    
+                    <div className='login_password_container'>
+                        <div className='login_password_info_container'>
+                            <label for='current-password'>Senha</label>
+                            <span>Amostrar</span>
+                        </div>
+                        <div className='login_password_input_container'>
+                            <input type='password' />
+                        </div>
+                    </div>
+                    
+                    <div className='login_button_container'> 
+                        <div className='login_button'>
+                            <Button variant="outline-dark" className='login_button'>Cadastrar</Button>
+                        </div>
+                    </div>
+                    
+                </form>
+            </div>
+        </div>
+        
     );
 }
 
