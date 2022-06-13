5a785fbf4d6e422c5086997e3cc497c7e439c5b4
CordeiroAboumrad <cordeiro.aboumrad@gmail.com>
2022-06-13 02:09:00 -0300

    Folders structure adjusted

---
+++ shared/index.js
@@ -0,0 +1,11 @@
+const ativos = require('./ativos.json');
+const carteira = require('./carteira.json');
+const watchlist = require('./watchlist.json');
+
+
+module.exports = () => ({
+  ativos: ativos,
+  carteira: carteira,
+  watchlist: watchlist
+  // Something more
+});
