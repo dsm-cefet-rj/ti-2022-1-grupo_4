2ad0e4a97558828943db3e679144aec9b9bc29c7
CordeiroAboumrad <cordeiro.aboumrad@gmail.com>
2022-06-12 21:20:43 -0300

    Arquivo AtivosSlice.js adicionado

---
+++ atlas-financeiro/src/store/slices/AtivosSlice.js
@@ -0,0 +1,25 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { useParams, useHistory } from "react-router-dom";
+
+export const fetchAtivos = createAsyncThunk('ativos/fetchAtivos',
+    async () => {
+        return await (await fetch('http://localhost:3004/ativos')).json();
+    });
+
+const ativosInicial = fetchAtivos;
+
+function fulfillAtivosReducer(ativosState, ativosFetched) {
+    return ativosFetched;
+}
+
+export const ativosSlice = createSlice({
+    name: 'ativos',
+    initialState: ativosInicial,
+    extraReducers: {
+        [fetchAtivos.fulfilled]: (state, action) => fulfillAtivosReducer(state, action.payload)
+    },
+})
+
+
+
+export default ativosSlice.reducer
