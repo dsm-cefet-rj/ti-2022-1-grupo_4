c6cecac20bd627ba956f5dd19d6af9b2e28061df
Jean Pierre Cordeiro Aboumrad <55934132+CordeiroAboumrad@users.noreply.github.com>
2022-06-15 20:46:26 -0300

    Update AtivosSlice.js

--- atlas-financeiro/src/store/slices/AtivosSlice.js
+++ atlas-financeiro/src/store/slices/AtivosSlice.js
@@ -2,25 +2,71 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { useParams, useHistory } from "react-router-dom";
 import { httpGet, httpPut, httpDelete, httpPost } from "../../utils";
 
+
+var ativosInitialState = {
+    status: 'not_loaded',
+    ativos: [],
+    error: null
+};
+
+function filterItems(state, arr) {
+    console.log('State');
+    console.log(state);
+    return state.ativos.filter(function(element) {
+        // debugger;
+        element.informacoes_gerais.ticker.includes(arr[1]);
+    });
+}
+
+function alteraBuscaReducer(state, busca) {
+    debugger
+    //alert('Filtro realizado com sucesso.');
+    //busca.busca = busca[1];
+    busca.ativos = filterItems(state, busca);
+    busca.status = 'filtered';
+    busca.error = null;
+    return busca;
+}
+
 export const fetchAtivos = createAsyncThunk('ativos/fetchAtivos',
     async () => {
-        return await (await fetch('http://localhost:3004/ativos')).json();
+        try{
+            const res = await (await fetch('http://localhost:3004/ativos')).json();
+            console.log(res);
+            return res;
+
+        } catch(error) {
+            return [];
+        }
     });
 
-const ativosInicial = [];
 
-function fulfillAtivosReducer(ativosState, ativosFetched) {
-    return ativosFetched.ativos;
+function fulfillAtivosReducer(state, ativosFetched) {
+    //console.log(`State (fulfillAtivosReducer): ${state}`)
+    //console.log(state);
+    state.status = 'loaded';
+    state.ativos = ativosFetched.ativos;
+
+    console.log('Completed');
+    console.log(state.ativos);
+    //console.log(state).ativos = ativosFetched;
+    // return ativosFetched;
 }
 
 export const ativosSlice = createSlice({
     name: 'ativos',
-    initialState: ativosInicial,
+    initialState: ativosInitialState,
+    reducers: {
+        alterarBusca: (state, action) => alteraBuscaReducer(state.ativos, action.payload)
+    },
     extraReducers: {
-        [fetchAtivos.fulfilled]: (state, action) => fulfillAtivosReducer(state, action.payload)
+        [fetchAtivos.fulfilled]: (state, action) => { fulfillAtivosReducer(state, action.payload) },
+        [fetchAtivos.pending]: (state, action) => {state.status = 'loading'},
+        [fetchAtivos.rejected]: (state, action) => {state.status = 'failed'; state.error = action.error.message}
     },
 })
 
 
+export const { alterarBusca } = ativosSlice.actions;
 
 export default ativosSlice.reducer
