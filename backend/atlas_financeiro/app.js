cc6dd4ddf9b6e7251842b69bc4e17e4f11e362b5
CordeiroAboumrad <cordeiro.aboumrad@gmail.com>
2022-06-18 01:22:04 -0300

    Conexão do front end com o back e adição do cors

--- backend/atlas_financeiro/app.js
+++ backend/atlas_financeiro/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -15,6 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    // console.log('Acessou o middleware.');
+    app.use(cors());
+    next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
