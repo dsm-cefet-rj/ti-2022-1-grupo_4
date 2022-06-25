b76aab4a6bdf329b06bc6b4ed4b544556d71ea34
mateussotero <49726164+mateussotero@users.noreply.github.com>
2022-06-25 15:33:27 -0300

    usuarios parte 1

--- backend/atlas_financeiro/app.js
+++ backend/atlas_financeiro/app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usuariosRouter = require('./routes/usuarios');
@@ -25,7 +26,16 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+//app.use(cookieParser());
+
+app.use(session({
+name: 'session-id',
+secret:'3456-6453-3456-9706',
+saveUninitialized:false,
+resave:false,
+store: new FileStore()
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
